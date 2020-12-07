e4ba535c0bfba81090460bbef46fff5a0530635a
vamshi188 <vamshi.ny188@gmail.com>
2020-12-06 23:34:05 -0500

    Added the Conditional operators

---
+++ src/com/vamshi/operatorsexample/ConditionalOperators.java
@@ -0,0 +1,21 @@
+package com.vamshi.operatorsexample;
+
+public class ConditionalOperators {
+    public static void main(String[] args) {
+        int age = 45;
+
+        System.out.println(age < 21 ? "you are old" : "you are the young man");
+
+//        int a;
+//        if (7 > 3) {
+//            a = 7;
+//        } else {
+//                a=3;
+//            }
+//        System.out.println(a);
+    }
+
+}
+// Conditional Operators all ways check with lift side value then right side
+// if lift side value true it's print to lift side or else it's print right side value
+// lift side value "you are old"  :   right side value  "you are the young man"
