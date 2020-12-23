706e9f97a8838cf4f7e4b7545c32a56b9fef17e4
vamshi188 <vamshi.ny188@gmail.com>
2020-12-22 23:08:40 -0500

    Added

---
+++ src/com/vamshi/forloopexample/ArrayforExample.java
@@ -0,0 +1,9 @@
+package com.vamshi.forloopexample;
+
+public class ArrayforExample {
+    public static void main(String[] args) {
+        int[] list = {25, 36, 85, 78};
+        for (int K : list)
+            System.out.println(K);
+    }
+}
