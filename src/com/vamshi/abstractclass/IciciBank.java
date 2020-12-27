921907f7194b89b427a70db04b05f44a65994d33
vamsi <vamshi.ny188@gmail.com>
2020-12-27 17:24:54 -0500

    add the new info for for loop

--- src/com/vamshi/abstractclass/IciciBank.java
+++ src/com/vamshi/abstractclass/IciciBank.java
@@ -2,11 +2,11 @@ package com.vamshi.abstractclass;
 
 public class IciciBank extends Bank {
 
-    void loan() {   // --> this method is implemented from Bank class bcz we need to create abstract class or implemented
+    public void loan() {   // --> this method is implemented from Bank class bcz we need to create abstract class or implemented
 
        System.out.println("Icic Bank Loan upto 8 lakhs");
     }
-    void interest() {  // --> this method is implemented from Bank class bcz we need to create abstract class or implemented
+   public void interest() {  // --> this method is implemented from Bank class bcz we need to create abstract class or implemented
 
         System.out.println("Icic Bank interest upto 5 lakhs");
     }
--- src/com/vamshi/forloopexample/foeExampleString.java
+++ src/com/vamshi/forloopexample/foeExampleString.java
@@ -1,11 +1,54 @@
 package com.vamshi.forloopexample;
 
-public class foeExampleString {
-        public static void main(String[] args) {
-            String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-            for (String string : cars) {
-                System.out.println(string);
-            }
-        }
-    }
+import java.util.ArrayList;
 
+//public class foeExampleString {
+//    public static void main(String[] args) {
+////        String[] cars = new String[4];
+////        cars[0]= "Volvo";
+////        cars[1] = "BWM";  //exact array  wants to print
+////        cars[2] = "Ford";
+////        cars[3] = "Mazada";
+////        System.out.println(cars[2]);
+////        }
+// another example
+//        ArrayList<String> carslist = new ArrayList<String>();
+//        carslist.add("Volvo");
+//        carslist.add("BMW");
+//        carslist.add("Ford");
+//        carslist.add("Mazda");
+//        System.out.println(carslist);
+//
+//
+////        for (int i = 0; i < carslist.size(); i++) {
+////            System.out.println(carslist.get(i));
+////        }
+//    }
+//
+//}
+
+
+import java.util.List;
+
+class JavaListOfListsMain {
+
+    public static void main(String[] args) {
+        List<ArrayList<String>> listOfLists = new ArrayList<ArrayList<String>>();
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Delhi");
+        list1.add("Mumbai");
+        listOfLists.add(list1);
+
+        ArrayList<String> anotherList = new ArrayList<String>();
+
+        anotherList.add("Beijing");
+        anotherList.add("Shanghai");
+        listOfLists.add(anotherList);
+
+        listOfLists.forEach((list)  ->
+                {
+                    list.forEach((city)->System.out.println(city));
+                }
+        );
+    }
+}
